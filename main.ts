caf10e02396de601960140dbbb0a9ae83064e473
Tsukasa (Real) <131947967+TsukasaFromPjskReal@users.noreply.github.com>
2023-05-02 09:33:34 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Catalina")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Maria Buzatu")
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+})
